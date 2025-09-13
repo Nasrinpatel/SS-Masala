4fdd9c30dd11542625cfcf7e759d1d1c889fa904
nasrinpatel <89184631+Nasrinpatel@users.noreply.github.com>
2025-09-13 20:56:47 +0530

    Organized Header and Footer separately

---
+++ footer.php
@@ -0,0 +1,190 @@
+ <!--==================================================-->
+    <!-- Start restho Footer Area -->
+    <!--==================================================-->
+    <div class="footer-area">
+      <video autoplay loop muted class="footer-inner-bg">
+        <source src="assets/images/video/footer-video.mp4" />
+      </video>
+      <div class="container">
+        <div class="row footer wow custom-anim-left">
+          <div class="col-lg-12">
+            <div class="footer-logo text-center">
+              <a href="index.html"
+                ><img src="assets/images/footer-logo.png" alt="logo"
+              /></a>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-2 col-md-6 col-sm-6">
+            <div class="foter-single-box wow custom-anim-left">
+              <div class="footer-wiget-title">
+                <h5>About</h5>
+              </div>
+              <div class="footer-wiget-menu">
+                <ul>
+                  <li><a href="index.html">Home</a></li>
+                  <li><a href="about.html">About Us</a></li>
+                  <li><a href="contact.html">Contact Us</a></li>
+                  <li><a href="food-menu.html">Our Menu</a></li>
+                  <li><a href="food-menu-2.html">Reservation</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-2 col-md-6 col-sm-6">
+            <div class="foter-single-box wow custom-anim-left">
+              <div class="footer-wiget-title">
+                <h5>Facilities</h5>
+              </div>
+              <div class="footer-wiget-menu">
+                <ul>
+                  <li><a href="menu.html">Private Functions</a></li>
+                  <li><a href="gallery.html">Menu Gallery</a></li>
+                  <li><a href="food-menu-2.html">Special Offers</a></li>
+                  <li><a href="popular-menu.html">Special Menu</a></li>
+                  <li><a href="food-menu.html">Weekend Menu</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6">
+            <div class="foter-single-box wow custom-anim-left">
+              <div class="footer-wiget-title">
+                <h5>Newsletters</h5>
+              </div>
+              <div class="footer-wiget-disc">
+                <p>
+                  Dolor sit amet consectetur adipiscing elit sed do eiusmod
+                  tempor incididunt labore et dolore
+                </p>
+              </div>
+              <form action="https://formspree.io/f/myyleorq" method="POST">
+                <div class="form-field">
+                  <input
+                    type="email"
+                    name="EMAIL"
+                    placeholder="Enter Your Email Address"
+                    required=""
+                  />
+                  <button class="submit-button" type="submit">
+                    <i class="bi bi-arrow-right"></i>
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6">
+            <div class="footer-wiget-quick-contanct wow custom-anim-left">
+              <div class="footer-wiget-title">
+                <h5>Contact</h5>
+              </div>
+              <div class="footer-wiget-contact-menu">
+                <ul>
+                  <li>
+                    <i class="fa-solid fa-location-dot"></i> 55 Main Street,
+                    D-Block, 2nd Floor, <span>Melbourne, Australia</span>
+                  </li>
+                  <li>
+                    <i class="fa-regular fa-envelope"></i>example@gmail.com
+                  </li>
+                  <li><i class="fa-solid fa-phone"></i> +012 (658) 69 000</li>
+                  <li><i class="fa-regular fa-clock"></i>09 am - 08 pm</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row footer-line align-items-center wow custom-anim-left">
+          <div class="col-lg-6 col-md-8">
+            <div class="copyright-text">
+              <p>Copyright @ 2025 <span>Restho</span> ALL Right Reserved</p>
+            </div>
+          </div>
+          <div class="col-lg-6 col-md-4">
+            <div class="footer-social-icon">
+              <ul>
+                <li>
+                  <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                </li>
+                <li>
+                  <a href="#"><i class="fa-brands fa-x-twitter"></i></a>
+                </li>
+                <li>
+                  <a href="#"><i class="fa-brands fa-youtube"></i></a>
+                </li>
+                <li>
+                  <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--==================================================-->
+    <!-- End restho Footer Area -->
+    <!--==================================================-->
+
+    <!--==================================================-->
+    <!-- Start scrollup section Area -->
+    <!--==================================================-->
+
+    <div id="progress" class="progress hide">
+      <div id="progress-value"></div>
+    </div>
+
+    <!--==================================================-->
+    <!-- Start scrollup section Area -->
+    <!--==================================================-->
+
+    <script src="assets/js/vendor/jquery-3.7.1.min.js"></script>
+
+    <script src="assets/js/odometer.min.js"></script>
+
+    <script src="assets/js/bootstrap.min.js"></script>
+
+    <script src="assets/js/imagesloaded.pkgd.min.js"></script>
+
+    <script src="assets/js/isotope.pkgd.min.js"></script>
+
+    <script src="venobox/venobox.js"></script>
+
+    <script src="venobox/venobox.min.js"></script>
+
+    <script src="assets/js/jquery.meanmenu.js"></script>
+
+    <script src="assets/js/jquery.scrollUp.js"></script>
+
+    <script src="assets/js/appear.js"></script>
+
+    <script src="assets/js/magnific-popup-js.html"></script>
+
+    <script src="assets/js/jquery.barfiller.js"></script>
+
+    <script src="assets/js/swiper-bundle.js"></script>
+
+    <script src="assets/js/owl.carousel.min.js"></script>
+
+    <script src="assets/js/theme.js"></script>
+    <script src="assets/js/wow.js"></script>
+    <script src="assets/js/my.js"></script>
+    <script src="assets/js/main.js"></script>
+    <!-- Owl Carousel Init -->
+    <script>
+    $(document).ready(function(){
+      $(".hero-slider").owlCarousel({
+        items:1,
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:4000,
+        smartSpeed:800,
+        nav:true,
+        dots:true
+      });
+    });
+
+    </script>
+  </body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,342 @@
+<!DOCTYPE html>
+<html lang="en-US">
+  
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <title>Hot Restho Restaurant HTML Template</title>
+    <meta name="description" content="" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <!-- Favicon -->
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="56x56"
+      href="assets/images/fav-icon/icon.png"
+    />
+    <!-- bootstrap CSS -->
+    <link
+      rel="stylesheet"
+      href="assets/css/bootstrap.min.css"
+      type="text/css"
+      media="all"
+    />
+
+    <!-- animate CSS -->
+    <link
+      rel="stylesheet"
+      href="assets/css/animate.css"
+      type="text/css"
+      media="all"
+    />
+
+    <!-- font-awesome CSS -->
+    <link
+      rel="stylesheet"
+      href="assets/css/all.min.css"
+      type="text/css"
+      media="all"
+    />
+    <!-- font-flaticon CSS -->
+    <link
+      rel="stylesheet"
+      href="assets/css/flaticon.css"
+      type="text/css"
+      media="all"
+    />
+    <!-- theme-default CSS -->
+    <link
+      rel="stylesheet"
+      href="assets/css/theme-default.css"
+      type="text/css"
+      media="all"
+    />
+
+    <!-- meanmenu CSS -->
+    <link
+      rel="stylesheet"
+      href="assets/css/meanmenu.min.css"
+      type="text/css"
+      media="all"
+    />
+    <!-- venobox CSS -->
+    <link
+      rel="stylesheet"
+      href="venobox/venobox.css"
+      type="text/css"
+      media="all"
+    />
+    <!-- bootstrap icons -->
+    <link
+      rel="stylesheet"
+      href="assets/css/bootstrap-icons.css"
+      type="text/css"
+      media="all"
+    />
+    <!-- magnific popup css -->
+    <link
+      rel="stylesheet"
+      href="assets/css/magnific-popup.css"
+      type="text/css"
+      media="all"
+    />
+
+    <!-- carousel css -->
+    <link
+      rel="stylesheet"
+      href="assets/css/owl.carousel.min.css"
+      type="text/css"
+      media="all"
+    />
+
+    <!-- Main Style CSS -->
+    <link
+      rel="stylesheet"
+      href="assets/css/style.css"
+      type="text/css"
+      media="all"
+    />
+    <link rel="stylesheet" href="assets/css/odometer-theme-default.css" />
+    <!-- responsive CSS -->
+    <link rel="stylesheet" href="assets/css/aos.css" />
+    <link
+      rel="stylesheet"
+      href="assets/css/responsive.css"
+      type="text/css"
+      media="all"
+    />
+    <link rel="stylesheet" href="assets/css/swiper-bundle.css" />
+    <!-- modernizr js -->
+    <style>
+      .logo_img img {
+          max-width: 100%;
+          height: auto;
+          max-height: 98px; 
+      }
+      .logo {
+          display: flex;
+          align-items: center;
+      }
+      .main_sticky img {
+          max-width: 100%;
+          height: auto;
+          max-height: 98px;
+      }
+      
+    </style>
+        <script src="assets/js/vendor/modernizr-3.5.0.min.js"></script>
+
+  </head>
+  <body>
+    <!-- loder -->
+    <div class="loader-wrapper">
+      <div class="loader"></div>
+      <div class="loder-section left-section"></div>
+      <div class="loder-section right-section"></div>
+    </div>
+
+    <!-- Curser Pointer -->
+    <div class="cursor"></div>
+    <div class="cursor2"></div>
+
+    <!--==================================================-->
+    <!-- Start restho Header top area style-two -->
+    <!--==================================================-->
+    <div class="header-top-area style-two">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-lg-9">
+            <div class="header-top-info">
+              <ul>
+                <li>
+                  <i class="fa-solid fa-envelope"></i
+                  ><span>MYSSMASALA@GMAIL.COM</span>
+                </li>
+                <li>
+                  <i class="fa-solid fa-location-dot"></i
+                  ><span> 1504 Washington Ave, United States</span>
+                </li>
+                <li>
+                  <i class="fa-solid fa-phone"></i><span> +91 9725820297</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-lg-3">
+            <div class="header-top-social">
+              <ul class="top-social-icon">
+                <li>
+                  <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                </li>
+                <li>
+                  <a href="#"><i class="fa-brands fa-x-twitter"></i></a>
+                </li>
+                <li>
+                  <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                </li>
+                <li>
+                  <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!--==================================================-->
+    <!-- End mediful Header top area -->
+    <!--==================================================-->
+
+    <!--==================================================-->
+    <!-- Start restho Main Menu Area -->
+    <!--==================================================-->
+    <div id="sticky-header" class="restho_nav_manu styl-two">
+      <div class="container">
+        <div class="row d-flex align-items-center">
+          <div class="col-lg-3">
+            <div class="logo">
+              <a class="logo_img" href="index.php" title="SS Masala">
+                <img src="assets/images/logo2.jpg" alt="logo" />
+              </a>
+              <a class="main_sticky" href="index.php" title="SS Masala">
+                <img src="assets/images/logo2.jpg" alt="astute" />
+              </a>
+            </div>
+          </div>
+          <div class="col-xl-9 col-lg-9">
+            <nav class="restho_menu">
+              <ul class="nav_scroll">
+                <li>
+                  <a href="#">Home<i class="fa-solid fa-angle-down"></i></a>
+                  <ul class="sub-menu">
+                    <li><a href="index.html">Home Version 01</a></li>
+                    <li><a href="index-2.html">Home Version 02</a></li>
+                    <li><a href="index-3.html">Home Version 03</a></li>
+                  </ul>
+                </li>
+                <li>
+                  <a href="#">Pages<i class="fa-solid fa-angle-down"></i></a>
+                  <ul class="sub-menu">
+                    <li><a href="about.html">About</a></li>
+                    <li><a href="service.html">Services</a></li>
+                    <li><a href="service-details.html">Services Details</a></li>
+                    <li><a href="team.html">Team</a></li>
+                    <li><a href="team-details.html">Team Details</a></li>
+                    <li><a href="testimonial.html">Testimonials</a></li>
+                    <li><a href="pricing.html">Pricing</a></li>
+                    <li><a href="contact.html">Contact</a></li>
+                    <li><a href="account.html">Account</a></li>
+                  </ul>
+                </li>
+                <li>
+                  <a href="#">Menu<i class="fa-solid fa-angle-down"></i></a>
+                  <ul class="sub-menu">
+                    <li><a href="food-menu.html">Menu List</a></li>
+                    <li><a href="food-menu-2.html">Food Menu</a></li>
+                    <li><a href="popular-menu.html">Menu Grid</a></li>
+                    <li><a href="menu.html">Choose Menu</a></li>
+                    <li><a href="gallery.html">Gallery Menu</a></li>
+                  </ul>
+                </li>
+                <li>
+                  <a href="#">Shop<i class="fa-solid fa-angle-down"></i></a>
+                  <ul class="sub-menu">
+                    <li><a href="shop.html">Shop</a></li>
+                    <li><a href="shop-details.html">Shop Details</a></li>
+                    <li><a href="cart.html">Shopping Cart</a></li>
+                    <li><a href="checkout.html">Checkout</a></li>
+                    <li><a href="account.html">Account</a></li>
+                  </ul>
+                </li>
+                <li>
+                  <a href="#">Blog<i class="fa-solid fa-angle-down"></i></a>
+                  <ul class="sub-menu">
+                    <li><a href="blog-grid.html">Blog Grid</a></li>
+                    <li><a href="blog-list.html">Blog List</a></li>
+                    <li><a href="blog-details.html">Blog Details</a></li>
+                  </ul>
+                </li>
+                <li><a href="contact.html">Contact</a></li>
+              </ul>
+              <div class="header-search-btn">
+                <div class="header-secrch-icon">
+                  <a class="search-box-btn search-box-outer" href="#"
+                    ><i class="bi bi-search"></i
+                  ></a>
+                </div>
+                <div class="restho-btn2">
+                  <a href="contact.html"
+                    >Contact Us <i class="fa-solid fa-arrow-right"></i
+                  ></a>
+                </div>
+              </div>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- restho Mobile Menu Area -->
+    <div class="mobile-menu-area sticky d-sm-block d-md-block d-lg-none">
+      <div class="mobile-menu">
+        <nav class="restho_menu">
+          <ul class="nav_scroll">
+            <li>
+              <a href="#">Home<i class="fa-solid fa-angle-down"></i></a>
+              <ul class="sub-menu">
+                <li><a href="index.html">Home Version 01</a></li>
+                <li><a href="index-2.html">Home Version 02</a></li>
+                <li><a href="index-3.html">Home Version 03</a></li>
+              </ul>
+            </li>
+            <li>
+              <a href="#">Pages<i class="fa-solid fa-angle-down"></i></a>
+              <ul class="sub-menu">
+                <li><a href="about.html">About</a></li>
+                <li><a href="service.html">Services</a></li>
+                <li><a href="service-details.html">Services Details</a></li>
+                <li><a href="team.html">Team</a></li>
+                <li><a href="team-details.html">Team Details</a></li>
+                <li><a href="testimonial.html">Testimonials</a></li>
+                <li><a href="pricing.html">Pricing</a></li>
+                <li><a href="contact.html">Contact</a></li>
+                <li><a href="account.html">Account</a></li>
+              </ul>
+            </li>
+            <li>
+              <a href="#">Menu<i class="fa-solid fa-angle-down"></i></a>
+              <ul class="sub-menu">
+                <li><a href="food-menu.html">Menu List</a></li>
+                <li><a href="food-menu-2.html">Food Menu</a></li>
+                <li><a href="popular-menu.html">Menu Grid</a></li>
+                <li><a href="menu.html">Choose Menu</a></li>
+                <li><a href="gallery.html">Gallery Menu</a></li>
+              </ul>
+            </li>
+            <li>
+              <a href="#">Shop<i class="fa-solid fa-angle-down"></i></a>
+              <ul class="sub-menu">
+                <li><a href="shop.html">Shop</a></li>
+                <li><a href="shop-details.html">Shop Details</a></li>
+                <li><a href="cart.html">Shopping Cart</a></li>
+                <li><a href="checkout.html">Checkout</a></li>
+                <li><a href="account.html">Account</a></li>
+              </ul>
+            </li>
+            <li>
+              <a href="#">Blog<i class="fa-solid fa-angle-down"></i></a>
+              <ul class="sub-menu">
+                <li><a href="blog-grid.html">Blog Grid</a></li>
+                <li><a href="blog-list.html">Blog List</a></li>
+                <li><a href="blog-details.html">Blog Details</a></li>
+              </ul>
+            </li>
+            <li><a href="contact.html">Contact</a></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+    <!--==================================================-->
+    <!-- End restho Main Menu Area -->
+    <!--==================================================-->
