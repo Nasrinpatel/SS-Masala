bf885cb72ee1ee583239b2dba91a4d8c1240ad71
fidaalivohra <30294306+fidaalivohra@users.noreply.github.com>
2026-03-13 22:21:01 +0530

    mobile menu fix

--- header.php
+++ header.php
@@ -253,6 +253,11 @@
 
     <!-- SS Masala Mobile Menu Area -->
     <div class="mobile-menu-area sticky d-sm-block d-md-block d-lg-none">
+      <div class="mobile-menu-brand">
+        <a href="index.php" title="SS Masala">
+          <img src="assets/images/logo.png" alt="SS Masala" />
+        </a>
+      </div>
       <div class="mobile-menu">
         <nav class="restho_menu">
           <ul class="nav_scroll">
@@ -262,10 +267,7 @@
                 <li><a href="#products">Products</a></li>
                 <li><a href="#inquiry">Inquiry</a></li>
                 <li><a href="#gallery">Gallery</a></li>
-                
-            </ul>
-            
-            <li><a href="#">Contact</a></li>
+                <li><a href="#inquiry">Contact</a></li>
           </ul>
         </nav>
       </div>
